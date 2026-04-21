9530792839e2b11458596bec08430812a00efa96
Workshop Participant <participant@example.com>
2026-04-21 13:34:55 +0000

    shell script updated

--- run_react_app.sh
+++ run_react_app.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-export AWS_REGION="${AWS_REGION:-${AWS_DEFAULT_REGION:-us-east-1}}"
+# #!/bin/bash
+# export AWS_REGION="${AWS_REGION:-${AWS_DEFAULT_REGION:-us-east-1}}"
+export AWS_REGION="${AWS_REGION:-$(curl -s -H "X-aws-ec2-metadata-token: $(curl -s -X PUT http://169.254.169.254/latest/api/token -H 'X-aws-ec2-metadata-token-ttl-seconds: 21600')" http://169.254.169.254/latest/meta-data/placement/region)}"
 export AGENT_ARN=$(aws bedrock-agentcore-control list-agent-runtimes --region "$AWS_REGION" \
   --query 'agentRuntimes[?status==`READY`].agentRuntimeArn | [0]' --output text)
 
